457e77a99cc48b02f16d203a4fc2794459e37a67
Mion giovanni <mion.ggb@gmail.com>
2015-07-02 00:55:39 +0200

    Ajout d'un commentaire pour tester GerritHub
    Change-Id: Idbe57d736fa722a5a23021331479a8db852bbc3a

--- Code/Common/common.testProject/src/main/java/common/testProject/GameModel.java
+++ Code/Common/common.testProject/src/main/java/common/testProject/GameModel.java
@@ -2,6 +2,11 @@ package common.testProject;
 
 import com.badlogic.gdx.Game;
 
+/**
+ * Game Model to start the test game.
+ * @author Bubul
+ *
+ */
 public class GameModel extends Game{
 
 	public static final int WIDTH = 800;
