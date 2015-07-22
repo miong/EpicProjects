09c41e629175d94272c33e030c5185796bce4413
AlexandreHostache <hostachealex@gmail.com>
2015-07-22 23:14:12 +0200

    J'ai corrigé le commentaire explicatif dans la classe game model
    Change-Id: Icf6720c88ddcce16ed9abcf9d4e95d4f4d6b96b4

--- Code/Common/common.testProject/src/main/java/common/testProject/gdxPart/GameModel.java
+++ Code/Common/common.testProject/src/main/java/common/testProject/gdxPart/GameModel.java
@@ -8,8 +8,15 @@ import com.badlogic.gdx.Game;
  *
  */
 public class GameModel extends Game{
-
+	
+	/**
+ 	*The width of the game window.
+	*/
 	public static final int WIDTH = 800;
+	
+	/**
+	*The height of the game window.
+	*/
 	public static final int HEIGHT = 400;
 
 	public void create() {
