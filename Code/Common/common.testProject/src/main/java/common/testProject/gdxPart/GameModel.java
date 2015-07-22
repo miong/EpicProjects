96e2937229da9999daf275ae6ed9e26a71eddd9d
AlexandreHostache <hostachealex@gmail.com>
2015-07-22 22:31:17 +0200

    J'ai ajouter un commentaire de type qualitatif dans la classe game model
    Change-Id: Icf6720c88ddcce16ed9abcf9d4e95d4f4d6b96b4

--- Code/Common/common.testProject/src/main/java/common/testProject/gdxPart/GameModel.java
+++ Code/Common/common.testProject/src/main/java/common/testProject/gdxPart/GameModel.java
@@ -8,7 +8,9 @@ import com.badlogic.gdx.Game;
  *
  */
 public class GameModel extends Game{
-
+/**
+ * Pélican is the best
+ */
 	public static final int WIDTH = 800;
 	public static final int HEIGHT = 400;
 
